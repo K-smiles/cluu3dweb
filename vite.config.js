36ca14cfb696611dc935504206e42025b56c15bc
cluu <lu452126871@gmail.com>
2024-04-23 21:44:54 +1000

    init prject

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+
+})
+
+// plugins: [react(), sentryVitePlugin({
+//   org: "jsm-x9",
+//   project: "javascript-react"
+// })],
+
+// build: {
+//   sourcemap: true
+// }
